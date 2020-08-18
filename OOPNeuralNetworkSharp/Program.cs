355ebd5ec411247038ad826923e79e69abf0f5f1
imaltont <sondre@unimicro.no>
2020-08-18 14:15:15 +0200

    Genetic algorithm

--- OOPNeuralNetworkSharp/Program.cs
+++ OOPNeuralNetworkSharp/Program.cs
@@ -20,12 +20,12 @@ namespace OOPNeuralNetworkSharp
             ActivationFunction eluFunc = (x) => { if (x > 0) return x; else return eluParam * (Math.Exp(x) - 1); };
             ActivationFunctionDerivative eluFuncDerivative = (x) => { if (x < 0) return x * eluParam * Math.Exp(x - 1); else return 1; };
 
-            bool isEA = true;
+            bool isEA = false;
             DataStruct[] trainingSet = new DataStruct[4];
-            trainingSet[0] = new DataStruct(new double[] { 1.0, 1.0 }, new double[] { 0.0 });
-            trainingSet[1] = new DataStruct(new double[] { -1.0, 1.0 }, new double[] { 1.0 });
-            trainingSet[2] = new DataStruct(new double[] { 1.0, -1.0 }, new double[] { 1.0 });
-            trainingSet[3] = new DataStruct(new double[] { -1.0, -1.0 }, new double[] { 0.0 });
+            trainingSet[0] = new DataStruct(new double[] { 1.0, 1.0 }, new double[] { 0.0, 1.0 });
+            trainingSet[1] = new DataStruct(new double[] { -1.0, 1.0 }, new double[] { 1.0, 0.0 });
+            trainingSet[2] = new DataStruct(new double[] { 1.0, -1.0 }, new double[] { 1.0, 0.0 });
+            trainingSet[3] = new DataStruct(new double[] { -1.0, -1.0 }, new double[] { 0.0, 1.0 });
 
             if (!isEA)
             {
@@ -37,7 +37,7 @@ namespace OOPNeuralNetworkSharp
                 network.AddLayer(sigmoidFunc, sigmoidFuncDerivative, 10);
                 network.AddLayer(eluFunc, eluFuncDerivative, 100);
                 network.AddLayer(sigmoidFunc, sigmoidFuncDerivative, 10);
-                network.AddLayer(sigmoidFunc, sigmoidFuncDerivative, 1);
+                network.AddLayer(sigmoidFunc, sigmoidFuncDerivative, 2);
                 network.BuildNetwork();
                 network.Train(trainingSet, 0.9, 100000, 1000);
 
@@ -77,9 +77,9 @@ namespace OOPNeuralNetworkSharp
                 LayerParams[] layers = new LayerParams[3];
                 layers[0] = new LayerParams(sigmoidFunc, sigmoidFuncDerivative, 10);
                 layers[1] = new LayerParams(sigmoidFunc, sigmoidFuncDerivative, 10);
-                layers[2] = new LayerParams(sigmoidFunc, sigmoidFuncDerivative, 1);
-                EA ea = new EA(10, 2, layers);
-                ea.EvolutionaryLoop(trainingSet, 10000, 5, 0.5, 0.7, 100, 0.3);
+                layers[2] = new LayerParams(sigmoidFunc, sigmoidFuncDerivative, 2);
+                EA ea = new EA(100, 2, layers);
+                ea.EvolutionaryLoop(trainingSet, 10000, 50, 0.5, 0.7, 10, 0.3);
                 var winner = ea.Population.OrderBy(x => x.Fitness).ThenBy(x => x.FromGeneration).ElementAt(0);
                 Console.WriteLine($"The best individual was created in generation: {winner.FromGeneration} and went through {winner.NumMutations} mutations");
 
