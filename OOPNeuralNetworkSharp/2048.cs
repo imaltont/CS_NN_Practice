f7f8a4ef52a74467058388ef2754e9e5b4c3e120
imaltont <sondrehegdal@protonmail.com>
2020-08-21 10:36:55 +0200

    2048 start

---
+++ OOPNeuralNetworkSharp/2048.cs
@@ -0,0 +1,15 @@
+//TODO: 
+//printing
+//game logic
+//fitnessfunction
+//random generation
+//find open spots for ^ (using tuples?)
+//DoAction
+namespace OOPNeuralNetworkSharp
+{
+    public class Game2048
+    {
+        public int[,] Board { get; set; }
+    }
+    
+}
